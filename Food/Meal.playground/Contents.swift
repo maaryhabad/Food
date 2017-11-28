63cd4b78ac478f85e57bd16d3f6b4e692bda7496
Mariana Beilune Abad <maryh.abad@gmail.com>
2017-11-28 21:07:58 -0200

    Playground

---
+++ Food/Meal.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Meal {
+    var happiness:Int?
+    var name:String?
+}
+
+var brownie = Meal()
+print(brownie.happiness)
+print(brownie.name)
--- Food/MyPlayground.playground/Contents.swift
+++ Food/MyPlayground.playground/Contents.swift
@@ -20,3 +20,34 @@ func add(name:String, calories:Double) {
 }
 add(name: "Brownie", calories: 30.5)
 add(name: "Eggplant", calories: 50.5)
+
+let calories2 = [50.5, 100, 300, 500, 450]
+
+for i in 0...3 { //i para contadores
+    print("calories are \(calories2[i])")
+}
+
+for i in 0...(calories2.count - 1) {
+    print(calories2[i])
+}
+
+for cal in calories2 { //cal(ou outro nome) é o valor interno da posição
+    print(cal)
+}
+
+// let empty: Array<tipo> = [] para declarar array vazio
+
+func allCalories(calories2:Array<Double>) -> Double { //retorna double (tem que explicitar o tipo do retorno)
+    var total = 0.0 //ou var total:Double = 0
+    for c in calories2 {
+        total += c
+    }
+    return total
+}
+
+var values = [1,2]
+var total = 0
+for v in values {
+    total += v
+}
+print (total / values.count)
